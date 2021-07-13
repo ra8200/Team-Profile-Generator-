ce88b1843b092921ff07627507ce64302b36a6c9
ra8200 <ra8200@hotmail.com>
2021-07-13 11:28:40 -0400

    fixed sytax on objects

--- classObjects/Engineer.js
+++ classObjects/Engineer.js
@@ -1,8 +1,8 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee{
-    constructor(name,id,email,gitHub){
-        super(name,id, email)
+    constructor(name, id, email, gitHub){
+        super(name, id, email)
         this.gitHub = gitHub;
     }
     getRole(){
